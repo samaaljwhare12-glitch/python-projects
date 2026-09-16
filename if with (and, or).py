f90cce65643462018965280d837f1484dc3d4b1d
Sama <samaaljwhare12@gmail.com>
2026-09-16 23:58:58 +0300

    Create if with (and, or).py

---
+++ if with (and, or).py
@@ -0,0 +1,8 @@
+username =input('Enter your username')
+password=input('Enter your password')
+if username == 'sama'and passwor=='1234' :
+	print ('Login successful')
+elif username =='sama' or password=='1234' :
+	print('One of the detalis is wrong')
+else :
+	print('Wrong username and password')
