d0c36869e2510ea9525395c4b2ab228d543c1860
Sama <samaaljwhare12@gmail.com>
2026-09-16 23:33:59 +0300

    Create nested if.py

---
+++ nested if.py
@@ -0,0 +1,10 @@
+gender='female'
+age=14
+if gender=='male' :
+	print('Gender : male')
+	if age<=21 :
+		print('He is young boy')
+elif gender == 'female' :
+	print ('Gender : female')
+	if age <=21 :
+		print ('She is young girl')
