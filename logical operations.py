294e6f2a2e473f77c8a127f46e7033511ff41add
Sama <samaaljwhare12@gmail.com>
2026-09-16 16:41:51 +0300

    Create logical operations.py

---
+++ logical operations.py
@@ -0,0 +1,17 @@
+x=5
+y=10
+print(10>9)
+
+#and
+print(10>9 and 20>7)
+print(10>9 and 20>90)
+print(10<9 and 20>90)
+
+#or
+print(10>2 or 20>90)
+print(10<2 or 20>90)
+print(10>2 or 20>90)
+
+#not
+print(not(10>2))
+print(not(9<3))
