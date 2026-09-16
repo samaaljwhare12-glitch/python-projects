e9946f76b930806582d06a5b302b0b6cc6628a47
Sama <samaaljwhare12@gmail.com>
2026-09-16 19:15:46 +0300

    Create if.py

---
+++ if.py
@@ -0,0 +1,26 @@
+name='sama'
+if name=='sama':
+	print('Your name is sama')
+	
+elif name =='kinda':
+      print('Your name is kinda');
+
+
+name='Ahmad'
+if name=='Ahmad':
+    print('your name is Ahmad')
+    
+elif name=='Ali':
+       print('your name is Ali')
+       
+elif name=='Sherief':
+       print('your name is Sherief')
+       
+else :
+       print ('sorry')
+       
+name='solafa'
+if name=='fadi':
+	print('your name is fadi')
+else:
+	print('sorry') 
