0a9a6983950018f668c5e7be3c0b06eca70168e1
Sama <samaaljwhare12@gmail.com>
2026-09-16 19:50:37 +0300

    Create calculator2.py

---
+++ calculator2.py
@@ -0,0 +1,29 @@
+num1 = float(input('Enter the first number: '))
+oper = input('Choose an operation (+, -, *, /, %): ')
+num2 = float(input('Enter the second number: '))
+
+if oper == '+':
+    result = num1 + num2
+    print(num1, '+', num2, '=', result)
+
+elif oper == '-':
+    result = num1 - num2
+    print(num1, '-', num2, '=', result)
+
+elif oper == '*':
+    result = num1 * num2
+    print(num1, '*', num2, '=', result)
+
+elif oper == '/':
+    if num2 != 0:
+        result = num1 / num2
+        print(num1, '/', num2, '=', result)
+    else:
+        print('cant division by zero')
+
+elif oper == '%':
+    result = num1 % num2
+    print(num1, '%', num2, '=', result)
+
+else:
+    print('Invalid operation', oper)
