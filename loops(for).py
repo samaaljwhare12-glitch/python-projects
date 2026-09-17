76b4fd374e4c282186cb595c7e67cf20cadad3ce
Sama <samaaljwhare12@gmail.com>
2026-09-17 15:57:58 +0300

    Create loops(for).py

---
+++ loops(for).py
@@ -0,0 +1,17 @@
+#sama aljwhari
+#for in
+for s in range (5):
+	print ('sama')
+	
+for s in range(1,11) :
+	print('s')
+	
+word ='python'
+for letter in word :
+	print(letter)
+	
+total=0
+for s in range (1,11) :
+	total=total+s
+	print('total=',total)
+	
